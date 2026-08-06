2f31ceb5bbaa17c3e3678e50fb2f68afe0359fb6
k-lindberg <kristin.n.lindberg@gmail.com>
2026-08-05 19:24:08 -0600

    week 2 day 2 / 4

--- js/modal.mjs
+++ js/modal.mjs
@@ -1,3 +1,8 @@
+import RebrickableAPI from "./RebrickableAPI.mjs";
+import { renderModal } from "./details.mjs";    
+    
+const rebrick = new RebrickableAPI();
+
 export async function openSetModal(setId) {
     const setDetails = await rebrick.getSetById(setId);
     const minifigs = await rebrick.getMinifigsForSet(setId);
