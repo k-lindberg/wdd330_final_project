de53be8b54fb8773da611acbecec8a131aaaaac7
k-lindberg <kristin.n.lindberg@gmail.com>
2026-08-10 17:37:44 -0600

    week 3 final 2

--- js/RebrickableAPI.mjs
+++ js/RebrickableAPI.mjs
@@ -26,21 +26,6 @@ export default class RebrickableAPI {
         return allThemes;
     }
 
-    // async getAllSets() {
-    //     let allSets = [];
-    //     let url = `${rebrickBaseURL}sets/?key=${rebrickKey}`;
-
-    //     while (url) {
-    //         const response = await fetch(url);
-    //         const data = await convertToJson(response);
-
-    //         allSets.push(...data.results);
-    //         url = data.next;
-    //     }
-
-    //     return allSets;
-    // }
-
     async getAllSetsByTheme(themeId) {
         let allSets = [];
         let url = `${rebrickBaseURL}sets/?theme_id=${themeId}&key=${rebrickKey}`;
@@ -56,49 +41,12 @@ export default class RebrickableAPI {
         return allSets;
     }
 
-    // async getSets(page = 1) {
-    //     const response = await fetch(`${rebrickBaseURL}sets/?page=${page}&key=${rebrickKey}`);
-        
-    //     return convertToJson(response);
-    // }
     async getSetById(setId) {
         const response = await fetch(`${rebrickBaseURL}sets/${setId}/?key=${rebrickKey}`);
         
         return convertToJson(response);
     }
-    // async getAllMinifigs() {
-    //     let allMinifigs = [];
-    //     let url = `${rebrickBaseURL}minifigs/?key=${rebrickKey}`;
-
-    //     while (url) {
-    //         const response = await fetch(url);
-    //         const data = await convertToJson(response);
-
-    //         allMinifigs.push(...data.results);
-    //         url = data.next;
-    //     }
-
-    //     return allMinifigs;
-    // }
-    // async getMinifigById(minifigId) {
-    //     const response = await fetch(`${rebrickBaseURL}minifigs/${minifigId}/?key=${rebrickKey}`);
-
-    //     return convertToJson(response);
-    // }
-    // async getAllPartsForSet(setId) {
-    //     let allParts = [];
-    //     let url = `${rebrickBaseURL}sets/${setId}/parts/?key=${rebrickKey}`;
-
-    //     while (url) {
-    //         const response = await fetch(url);
-    //         const data = await convertToJson(response);
-
-    //         allParts.push(...data.results);
-    //         url = data.next;
-    //     }
-
-    //     return allParts;
-    // }
+    
     async getMinifigsForSet(setId) {
         let allMinifigs = [];
         let url = `${rebrickBaseURL}sets/${setId}/minifigs/?key=${rebrickKey}`;
